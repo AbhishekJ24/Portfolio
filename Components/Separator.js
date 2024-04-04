75b7a5866c6d864e9c43d971205f2f33eccb4e85
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-04-04 23:53:12 +0530

    Installed framer-motion, styled projects page

--- Components/Separator.js
+++ Components/Separator.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Separator = () => {
   return (
-    <div className='h-32 w-full'>
+    <div className='h-8 w-full'>
     </div>
   )
 }
--- app/Projects/page.js
+++ app/Projects/page.js
@@ -8,26 +8,26 @@ const projects = () => {
 
         <div id='project-cont' className='flex flex-wrap gap-10 justify-around'>
 
-          <div className='project-card bg-slate-300 rounded-2xl w-80 h-96 p-5 overflow-y-clip flex flex-col'>
-            <img src='hybridmodel.avif' className='object-cover h-32 w-72 rounded-2xl hover:scale-105' />
-            <p className='mt-5 text-black font-semibold'> Hybrid model for accurate prediction of increasing temporal big data in the cloud for mining large collections of co-evolving sequences </p>
-            <Link className='text-sm p-3 rounded-lg bg-orange-950 hover:bg-orange-900 mt-5 w-[10rem]' href='https://github.com/AbhishekJ24/Hybrid-Model-for-Mining-on-Co-Evolving-Sequences.git'>
+          <div className='project-card rounded-2xl p-5 overflow-y-clip flex flex-col'>
+            <img src='hybridmodel.png' className='object-cover rounded-2xl hover:scale-105' />
+            <p className='mt-5 font-bold'> Hybrid model for accurate prediction of increasing temporal big data in the cloud for mining large collections of co-evolving sequences </p>
+            <a className='text-sm p-3 rounded-lg text-white mt-5 w-[10rem] font-semibold' href='https://github.com/AbhishekJ24/Hybrid-Model-for-Mining-on-Co-Evolving-Sequences.git'>
               GitHub Repository
-            </Link>
+            </a>
           </div>
-          <div className='project-card bg-slate-300 rounded-2xl h-96 w-80 p-5 overflow-y-clip flex flex-col'>
-            <img src='llm.jpg' className='object-cover h-32 w-72 rounded-2xl hover:scale-105' />
-            <p className='mt-5 text-black font-semibold'> SummaEase: Text and Speech Summarization using BERT LLM </p>
-            <Link className='text-sm p-3 rounded-lg bg-orange-950 hover:bg-orange-900 mt-5 w-[10rem]' href='https://github.com/AbhishekJ24/SummaEase-Text-And-Speech-Summarizer-using-LLM.git'>
+          <div className='project-card rounded-2xl p-5 overflow-y-clip flex flex-col'>
+            <img src='llm.jpg' className='object-cover rounded-2xl hover:scale-105' />
+            <p className='mt-5 font-bold'> SummaEase: Text and Speech Summarization using BERT LLM </p>
+            <a className='text-sm p-3 rounded-lg text-white mt-5 w-[10rem] font-semibold' href='https://github.com/AbhishekJ24/SummaEase-Text-And-Speech-Summarizer-using-LLM.git'>
               GitHub Repository
-            </Link>
+            </a>
           </div>
-          <div className='project-card bg-slate-300 rounded-2xl h-96 w-80 p-5 overflow-y-clip flex flex-col'>
-            <img src='linguocheck.png' className='object-cover h-32 w-72 rounded-2xl hover:scale-105' />
-            <p className='mt-5 text-black font-semibold'> LinguoCheck: NLP Grammar Checker using Python and Flask </p>
-            <Link className='text-sm p-3 rounded-lg bg-orange-950 hover:bg-orange-900 mt-5 w-[10rem]' href='https://github.com/AbhishekJ24/LinguoCheck-NLP-Grammar-Checker.git'>
+          <div className='project-card rounded-2xl p-5 overflow-y-clip flex flex-col'>
+            <img src='linguocheck.png' className='object-cover rounded-2xl hover:scale-105' />
+            <p className='mt-5 font-bold'> LinguoCheck: NLP Grammar Checker using Python and Flask </p>
+            <a className='text-sm p-3 rounded-lg text-white mt-5 w-[10rem] font-semibold' href='https://github.com/AbhishekJ24/LinguoCheck-NLP-Grammar-Checker.git'>
               GitHub Repository
-            </Link>
+            </a>
           </div>
         </div>
       </div>
