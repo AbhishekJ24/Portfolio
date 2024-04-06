a219c46ea43231f00ab75e8b2ad005c6e7f63526
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-04-06 23:33:21 +0530

    Header use client added

--- Components/Header.js
+++ Components/Header.js
@@ -1,20 +1,19 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
 
 const Header = () => {
   return (
     <>
-      <div>
-        <div className='flex justify-between p-10'>
-          <div className=''>
-            <a href='/'> <img id='logo' className='invert h-12' src='logo.png' alt='not found' /> </a>
-          </div>
-          <div className='flex items-center gap-12'>
-            <Link className='hover:scale-110' href="/">Home</Link>
-            <Link className='hover:scale-110' href='/Projects'>Projects</Link>
-            <Link className='hover:scale-110' href='/Experience'>Experience</Link>
-            <Link className='hover:scale-110' href='/Contact'>Contact</Link>
-          </div>
+      <div className='flex justify-between p-10'>
+        <div className=''>
+          <a href='/'> <img id='logo' className='invert h-12' src='logo.png' alt='not found' /> </a>
+        </div>
+        <div className='flex items-center gap-12'>
+          <Link className='hover:scale-110' href="/">Home</Link>
+          <Link className='hover:scale-110' href='/Projects'>Projects</Link>
+          <Link className='hover:scale-110' href='/Experience'>Experience</Link>
+          <Link className='hover:scale-110' href='/Contact'>Contact</Link>
         </div>
       </div>
     </>
